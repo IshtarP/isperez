341e52883d83e55e727c8f98fabf1a638eeef74c
Ishtar Perez <isperez@csumb.edu>
2018-02-04 02:02:32 +0000

    changed default page

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hello !! My name is Ishtar Perez!
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+header("./isperez/Lab1");
+
+?>
+</body>
+</html>

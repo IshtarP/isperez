dec3c3fd173b8b6301d191ae9123c9c204967aac
Ishtar Perez <isperez@csumb.edu>
2018-04-24 18:51:07 +0000

    Homework4

--- Homeworks/Homework4/js/functions.js
+++ Homeworks/Homework4/js/functions.js
@@ -1,6 +1,6 @@
 
 
-hands = ["paper.png","rock.png","scissors.png"]
+var hands = ["paper.png","rock.png","scissors.png"]; // An array of the pictures used
 
 function getRandomInt(min, max) {
          return Math.floor(Math.random() * (max - min + 1) + min);
@@ -25,33 +25,33 @@ $("#button").click( function()  {
     
  
     
-    // ghetto way to shuffle
+    //  Way to shuffle
     
-    player1 = hands[getRandomInt(0,hands.length - 1)]
-    player2 = hands[getRandomInt(0,hands.length - 1)]
+    var player1 = hands[getRandomInt(0,hands.length - 1)]
+    var player2 = hands[getRandomInt(0,hands.length - 1)]
     
     while(player1 === player1prev && player2  === player2prev) {
             player1 = hands[getRandomInt(0,hands.length - 1)]
             player2 = hands[getRandomInt(0,hands.length - 1)]
     }
     
-    player1prev = player1
-    player2prev = player2
+    player1prev = player1;
+    player2prev = player2;
 
 
     
-    player1Hand = '<img src="./img/' + player1 + '"/>'
-    player2Hand = '<img src="./img/' + player2 + '"/>'
+    var player1Hand = '<img src="./img/' + player1 + '"/>'; // Displays the image for player 1
+    var player2Hand = '<img src="./img/' + player2 + '"/>'; // Displays the image for player 2
     
     
     
     
         if (player1Hand === player2Hand) {
-        $("#outcome").append("<h3> Tie </h3>"); 
+        $("#outcome").append("<h3> Tie </h3>");  // Print a Tie
         
     } else {
         var player1Wins = false; // variable for player 1 wins is set to false
-        if(player1 === 'rock.png' && player2 == 'scissors.png') {
+        if(player1 === 'rock.png' && player2 == 'scissors.png') { // Using images to compare 
             player1Wins = true;
         } else if (player1 === 'paper.png' && player2 === 'rock.png') {
             player1Wins = true;
@@ -60,88 +60,16 @@ $("#button").click( function()  {
         }
         
         if(player1Wins) {
-            $("#outcome").append("<h2> Player 1 Wins!! </h2> ");
+            $("#outcome").append("<h2> Player 1 Wins!! </h2> "); 
         } else {
             $("#outcome").append("<h2> Player 2 Wins!! <h2> ");
         }
     }
     
     
-    $('#playArea').append(player1Hand)
-      $('#playArea').append(player2Hand)
+    $('#playArea').append(player1Hand) //The append() method inserts the playerHand to the div with an id playArea
+      $('#playArea').append(player2Hand) // The append() method inserts the playerHand to the div with an id playArea
 
 });
 
 
-/*
-
-
-
-function handSigns(randomValue) {
-    
-    var fileName = "";
-    switch (randomValue) {
-        case 0: 
-            fileName = "rock";
-                break;
-        case 1: 
-            fileName = "paper";
-                break;
-                
-        case 2:
-            fileName = "scissors";
-                break;
-        
-        default:
-            // code
-    }
-    // Diplay image
-}
-
-function displayHands(player1, player2) {
-    var rock = 0;
-    var paper = 1;
-    var scissors = 2;
-    
-    document.getElementById("div").innerHTML;
-    
-    if (player1 == player2) {
-        $("#tie").append("<h3> Tie </h3>"); // Outputs if it is a tie
-        
-    } else {
-        var player1Wins = false; // variable for player 1 wins is set to false
-        if(player1 == rock && player2 == scissors) {
-            player1Wins = true;
-        } else if (player1 == paper && player2 == rock) {
-            player1Wins = true;
-        } else if (player1 == scissors && player2 == paper) {
-            player1Wins = true;
-        }
-        
-        if(player1Wins) {
-            $("#wins").append("<h2> Player 1 Wins!! </h2> ");
-        } else {
-            $("#wins").append("<h2> Player 2 Wins!! <h2> ");
-        }
-    }
-    
-}
-
-function drawing() {
-    var players = new Array(); // Creates a new array
-    
-   for (var i =0; i < 2; i++) {
-       var randomChoice = Math.floor(Math.random() * 4);
-       
-       handSigns()
-       players.push(players, randomChoice);
-       
-       
-   }
-   
-   displayHands(players[0], players[1]);        
-    
-    
-}
-
-*/

5295b3d6cbca88f16f65c062ec833091e504280e
Ishtar Perez <isperez@csumb.edu>
2019-04-12 05:57:31 +0000

    Laptop website

---
+++ JavaScriptPractice/JavaScript/todo.js
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>JavaScript Introduction </title>
+    </head>
+    <body>
+        <h1>JavaScript</h1>
+
+    </body>
+</html>
--- labs/Lab8/hangman/js/hangman.js
+++ labs/Lab8/hangman/js/hangman.js
@@ -1,9 +1,9 @@
 // Variables
-var selectedWord = "";
-var selectedHint = "";
-var board = [];
-var remainingGuesses = 6;
-var words = [{ word:"snake", hint: "It's a reptile" },
+var selectedWord = ""; // Set to empty string
+var selectedHint = ""; // Set to empty string
+var board = []; // Empty array 
+var remainingGuesses = 6; // Setting the remaining guesses to 6
+var words = [{ word:"snake", hint: "It's a reptile" }, // An array with key values
              { word:"monkey", hint: "It's a mammal"},
              { word:"beetle", hint: "It's an insect" },
              { word:"lion", hint: "It's a mammal"},
@@ -15,28 +15,34 @@ var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
                 
 
-
+// Set the count to 0
 var count = 0
 
+// the given hint is set to false
 var hintGiven = false
 
              
 // LISTENER
+// check the visitor's browser type and browser version, and load the proper version of the web page based on the information.
+//The onload event can also be used to deal with cookies
 window.onload = startGame();
 
-$(".letter").click( function() {
-    checkLetter($(this).attr("id"));
+// The function is executed when the user clicks on the HTML element.
+$(".letters").click( function() {
+    checkLetter($(this).attr("id")); //method is used to get attribute values
     disableButton($(this));
 });
 
+// The reload() method is used to reload the current document.
+//The reload() method does the same as the reload button in your browser.
 $(".replayBtn").on("click", function() {
-    location.reload();
+    location.reload();  //The location object contains information about the current URL.
 });
 
 $(".theHint").click( function () {
-    console.log(Math.floor(Math.random() * 10))
+    console.log(Math.floor(Math.random() * 10)) // Random hints
     
-      checkLetter($(this).attr("id"));
+      checkLetter($(this).attr("id")); // check for a hint 
       $('#aHint').show()
 
   
@@ -48,6 +54,7 @@ $(".theHint").click( function () {
 
 
 // Functions
+// That calls every other function
 function startGame() {
     pickWord(); 
     initBoard();
@@ -57,6 +64,7 @@ function startGame() {
 
 }
 
+// it inititates the borard that shows the underscores
 function initBoard() {
     for (var letter in selectedWord) {
         board.push("_");
@@ -64,6 +72,7 @@ function initBoard() {
 }
 
 // Create the letters inside the letters div
+// Letters are display in green as it is the color of the btn-success 
 function createLetters() {
     for (var letter of alphabet) {
         $("#letters").append("<button  class='btn btn-success letter' id='" + letter + "'>"  + letter +  "</button>");
@@ -71,52 +80,52 @@ function createLetters() {
 }
 
 
+// function that picks word 
 function pickWord() {
-    var randomInt = Math.floor(Math.random() * words.length);
-    selectedWord = words[randomInt].word.toUpperCase(); 
-    selectedHint = words[randomInt].hint;
+    var randomInt = Math.floor(Math.random() * words.length); // randomize the words
+    selectedWord = words[randomInt].word.toUpperCase(); // upper case the words
+    selectedHint = words[randomInt].hint; // randomize the hint as well 
 }
 
 // Checks to see if the selected letter exist in the selected Word
 function checkLetter(letter) {
-    var positions = new Array();
+    var positions = new Array(); // create a new array called positions
     
-    for (var i=0; i < selectedWord.length; i++) {
-        if (letter == selectedWord[i]) {
+    for (var i=0; i < selectedWord.length; i++) { // loop over the length of the selected words
+        if (letter == selectedWord[i]) { // if the letter is equal to the selected word then push[i]
             positions.push(i);
 
         }
     }
     
-    if (positions.length > 0) {
+    if (positions.length > 0) { // if the lenght of the position is greater than 0 then add to the count the length of the position
         count += positions.length
-        updateWord(positions, letter);
+        updateWord(positions, letter); // update the positions and letter
     } else {
-        remainingGuesses -= 1;
+        remainingGuesses -= 1; // else take away a guess
     }
     
-    if (remainingGuesses <= 0 ) {
-        endGame(false);
+    if (remainingGuesses <= 0 ) { // if remaining guesses is less than or equal to 0
+        endGame(false); // we end the game
     }
     
-    if(count === selectedWord.length)
-        endGame(true)
+    if(count === selectedWord.length) // if the count is equal to the selected word lenght then 
+        endGame(true) // continue the game
     
 
 
-    updateMan();
-    
+    updateMan(); // calling update the man function
 }
 
 // Update the current word then calls for a board update
 function updateWord(positions, letter) {
     for (var pos of positions) {
-        board[pos] = letter;
+        board[pos] = letter; // for pos in posotion board position will equal the letter
     }
     
-    updateBoard();
+    updateBoard(); // update the board
     
-    if (!board.includes('_')) {
+    if (!board.includes('_')) { // if not equal to reaminibg _ then 
         endGame(true);
     }
 } 
@@ -125,7 +134,7 @@ function updateBoard() {
     
     $("#word").empty();
    
-    for(var i=0; i < board.length; i++) {
+    for(var i=0; i < board.length; i++) { // for loop that goes over the lenght of the board
        $("#word").append(board[i] + " ");
     
     }
